94255492df5c1e2d3a9beec9856b74768c7e4170
shiyangyang <shiyangyang@flowpp.com>
2018-03-05 11:44:38 +0800

    README.md

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-# ipreact
+# IPreact
 connect data-actions of preact-components via immutablejs
+
+## Install
+```
+npm i ipreact
+```
+
+## Usage
+```tsx
+import IPreact from 'ipreact'
+import { h, Component, render } from 'preact'
+const { connect, dispatch, getState } = IPreact({
+    name: 'world'
+})
+
+const App = connect(() => ({
+    words: `hello ${getState().getIn(['name'])}!`
+}))(({ words }) => <h2>{words}</h2>)
+
+let i = 0
+setInterval(function () {
+    const list = ['prect', 'immutable', 'world', 'ipreact']
+     i = (i + 1) % list.length
+    dispatch(state => state.setIn(['name'], list[i]))
+}, 1000)
+```
+![hello world](./helloworld.gif)
--- test/index.tsx
+++ test/index.tsx
@@ -1,41 +1,20 @@
 import IPreact from '../src/index'
 import { h, Component, render } from 'preact'
-
-const store = IPreact({
-    pause: false,
-    time: new Date
+const { connect, dispatch, getState } = IPreact({
+    name: 'world'
 })
-const { connect, dispatch, getState  } = store
 
-const TimeShow = (props) => {
-    const { time, pause, tooglePause } = props
-    return <div>
-        <i>{time.getFullYear()}</i>年
-        <i>{time.getMonth() + 1}</i>月
-        <i>{time.getDate()}</i>日
-        <i>{time.getHours()}</i>时
-        <i>{time.getMinutes()}</i>分
-        <i>{time.getSeconds()}</i>秒
-        <button onClick={tooglePause}>{pause ? '开始' : '暂停'}</button>
-    </div>
-}
+const App = connect(() => ({
+    words: `hello ${getState().getIn(['name'])}!`
+}))(({ words }) => <h2>{words}</h2>)
 
-const Application = connect(() => {
-    const state = getState()
-    return {
-        time: state.getIn(['time']),
-        pause: state.getIn(['pause']),
-        tooglePause: () => dispatch(state => state.updateIn(['pause'], p => !p))
-    }
-})(TimeShow)
-
+let i = 0
 setInterval(function () {
-    const state = getState()
-    if (!state.getIn(['pause'])) {
-        dispatch(state => state.setIn(['time'], new Date))
-    }
+    const list = ['prect', 'immutable', 'world', 'ipreact']
+     i = (i + 1) % list.length
+    dispatch(state => state.setIn(['name'], list[i]))
 }, 1000)
 
-export default ({ el }: {el: HTMLElement}) => {
-    render(<Application />, el)
+export default ({ el }: { el: HTMLElement }) => {
+    render(<App />, el)
 }
